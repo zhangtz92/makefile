9a8e8fe9c066ba347e40f5b664d5140f3e994147
zhangtz92 <752026379@qq.com>
2021-10-24 16:14:17 +0900

    mov

--- assembly.py
+++ assembly.py
@@ -7,3 +7,31 @@ FETCH=[pin.PC_OUT | pin.MAR_IN ,
         pin.PC_OUT | pin.MAR_IN ,
         pin.MC_OUT | pin.SRC_IN | pin.PC_INC,
 ]
+
+MOV=0|(1<<7)
+ADD=(1<<4)|(1<<7)
+HLT=0x3f
+NOP=0
+
+INSTRUCTIONS={
+        2:{
+                MOV:{
+                        #MOV A 5,前面寄存器，后面立即数
+                        #需要将原操作数寄存器SRC中值移到目的操作寄存器DST中表示的寄存器里
+                        (pin.AM_REG,pin.AM_INS):[
+                                pin.DST_W | pin.SRC_OUT
+                        ]
+                }
+        },
+        1:{},
+        0:{
+                HLT:[
+                        pin.HLT
+                ],
+                NOP:[
+                        pin.PCC_RD
+                ]
+
+        }
+}
+#print(MOV)
